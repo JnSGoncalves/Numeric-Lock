54805ad526efe69fe981274b5a5411c0dd92fdc3
Generoso2 <161092418+Generoso2@users.noreply.github.com>
2025-04-14 13:42:50 +0000

    timer de 5 segundos para quando for digitada uma senha incorreta

--- main.asm
+++ main.asm
@@ -1,7 +1,7 @@
            ORG 0000H
            LJMP Main
 
-pSenha EQU 50h
+pSenha   EQU 50h
 pEntrada EQU 60h
 
            ORG 0100H
@@ -16,18 +16,21 @@ Main:
 				MOV R6, #pEntrada
 				MOV R4, #06h
 				CALL Compare
-				
-				
 
-				JMP $				
+				CJNE R3, #00h, SenhaErrada
 
+				; Senha correta - colocar aqui a lógica que quiser (ex: abrir menu, acender LED, etc.)
+				JMP $
+
+SenhaErrada:
+				CALL Delay5s
+				JMP Main
+
+; ---------------- COMPARAÇÃO DE SENHAS ----------------
 Compare:
-		; R7 ponteiro de inicio do array correto
-		; R6 ponteiro de inicio do array p/ comparar
-		; R2 contador do indice
-		; R3 resposta (#01h incorreto/#00h correto)
-		; R1 e R0, usados p/ endereçamento
-		; R4 tamanho do array que quer comparar
+				; R7 ponteiro senha correta
+				; R6 ponteiro senha digitada
+				; R4 tamanho da senha
 				MOV R2, #0H
 				MOV R3, #0H
 		CompLoop:
@@ -54,8 +57,8 @@ Compare:
 				JNZ CompLoop
 		compReturn:
 				RET
-				
 
+; ---------------- ENTRADA VIA TECLADO ----------------
 Teclado:
 				MOV A, #0H
 				MOV R5, #0H
@@ -64,53 +67,70 @@ Teclado:
 				CJNE R5, #06h, Loop
 				RET
 
-		Linha:
-				MOV R0, #01 ; clear R0 - the first key is key1
+Linha:
+				MOV R0, #01 ; valor da tecla
 
-				; scan row0
-				SETB P0.0 ; set row3
-				CLR P0.3 ; clear row0
-				CALL colScan ; call column-scan subroutine
+				; Linha 0
+				SETB P0.0
+				CLR P0.3
+				CALL colScan
 
-          		; scan row1
-				SETB P0.3 ; set row0
-				CLR P0.2 ; clear row1
-				CALL colScan ; call column-scan subroutine
+				; Linha 1
+				SETB P0.3
+				CLR P0.2
+				CALL colScan
 
-				; scan row2
-				SETB P0.2; set row1
-				CLR P0.1 ; clear row2
-				CALL colScan ; call column-scan subroutine
+				; Linha 2
+				SETB P0.2
+				CLR P0.1
+				CALL colScan
 
-				; scan row3
-				SETB P0.1 ; set row2
-				CLR P0.0 ; clear row3
-				CALL colScan ; call column-scan subroutine
+				; Linha 3
+				SETB P0.1
+				CLR P0.0
+				CALL colScan
 
-				RET				
+				RET
 
-		colScan:
-				JNB P0.6, gotKey ; if col0 is cleared - key found
-				INC R0 ; otherwise move to next key
-				JNB P0.5, gotKey ; if col1 is cleared - key found
-				INC R0 ; otherwise move to next key
-				JNB P0.4, gotKey ; if col2 is cleared - key found
-				INC R0 ; otherwise move to next key
-				RET ; return from subroutine - key not found
-		gotKey:
+colScan:
+				JNB P0.6, gotKey
+				INC R0
+				JNB P0.5, gotKey
+				INC R0
+				JNB P0.4, gotKey
+				INC R0
+				RET
+
+gotKey:
 				MOV A, R0
 				MOV R2, A
-		guardar:		
+guardar:
 				MOV A, R6
 				ADD A, R5
 				MOV R1, A
 				MOV A, R2
 				MOV @R1, A
 				INC R5
-		espera: 
-;Espera a tecla ser desprecionada para contar outra
+
+espera:
 				JNB P0.6, espera
 				JNB P0.5, espera
 				JNB P0.4, espera
-				
+				RET
+
+; ---------------- DELAY DE 5 SEGUNDOS ----------------
+Delay5s:
+				MOV R1, #5 ; repete 5 vezes delay de ~1s
+
+DelayLoop1s:
+				MOV R2, #250
+Loop1:
+				MOV R3, #255
+Loop2:
+				MOV R4, #255
+Loop3:
+				DJNZ R4, Loop3
+				DJNZ R3, Loop2
+				DJNZ R2, Loop1
+				DJNZ R1, DelayLoop1s
 				RET
